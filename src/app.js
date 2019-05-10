edc52e3a0060a3dc46fbdc11a6efbd78fa17283d
Ryan Carville <ryancarville@gmail.com>
2019-05-10 16:00:49 +0200

    API and Router in place

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helemt = require('helmet');
 const { NODE_ENV } = require('./config');
+const logger = require('./logger');
+const bookmarks = require('./bookmarks/addBookmark');
 
 const app = express();
 
@@ -11,6 +13,19 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helemt());
+
+app.use(function validateBearerToken(req, res, next) {
+	const apiToken = process.env.API_TOKEN;
+	const authToken = req.get('Authorization');
+
+	if (!authToken || authToken.split(' ')[1] !== apiToken) {
+		logger.error(`Unauthorized request to path: ${req.path}`);
+		return res.status(401).json({ error: 'Unauthorized request' });
+	}
+	next();
+});
+
+app.use(bookmarks);
 
 app.get('/', (req, res) => {
 	res.send('Hello, boilerplate!');
---
+++ src/bookmarks/addBookmark.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const uuid = require('uuid/v4');
+const logger = require('../logger');
+const bookmarkRouter = express.Router();
+const bodyParse = express.json();
+
+const { bookmarks } = require('../store');
+
+bookmarkRouter
+	.route('/bookmarks')
+	.get((req, res) => {
+		res.json(bookmarks);
+	})
+	.post(bodyParse, (req, res) => {
+		const { title, url, description, rating } = req.body;
+
+		if (!title) {
+			logger.error('Title Required');
+			return res.status(400).send('Title Required');
+		}
+
+		if (!url) {
+			logger.error('URL required');
+			return res.status(400).send('URL required');
+		}
+
+		const id = uuid();
+		const bookmark = {
+			id,
+			title,
+			url,
+			description,
+			rating
+		};
+		bookmarks.push(bookmark);
+		logger.info(`Bookmark with id ${id} created`);
+		res
+			.status(201)
+			.location(`http://localhost:8000/bookmarks/${id}`)
+			.json(bookmark);
+	});
+
+bookmarkRouter
+	.route('/bookmarks/:id')
+	.get((req, res) => {
+		const { id } = req.params;
+		const bookmark = bookmarks.find(bm => bm.id == id);
+
+		if (!bookmark) {
+			logger.error('Bookmark with id ${id} not found.');
+			return res.status(404).send('Bookmark Not Found');
+		}
+		res.json(bookmark);
+	})
+	.delete((req, res) => {
+		const { id } = req.params;
+		const bookmarkIndex = bookmarks.findIndex(bm => bm.id == id);
+		if (bookmarkIndex === -1) {
+			logger.error(`Bookmark with id: ${id} not found`);
+			return res.status(404).send('Bookmark not found');
+		}
+
+		bookmarks.forEach(bookmark => {
+			const bmId = bookmark.filter(bm => bm.id !== id);
+			bookmark.id = bmId;
+		});
+
+		bookmarks.splice(bookmarkIndex, 1);
+		logger.info(`Bookmark with id ${id} deleted`);
+		res.send.status(204).end();
+	});
+
+module.exports = bookmarkRouter;
